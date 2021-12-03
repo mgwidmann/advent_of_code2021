49d7dacf0149f3a7f0da8863f94c8f35a4f12c5d
matt <mgwidmann@gmail.com>
2021-12-02 22:30:31 -0500

    Day 2 part 2

--- lib/advent_of_code2021/problems/day2.ex
+++ lib/advent_of_code2021/problems/day2.ex
@@ -1,22 +1,22 @@
 defmodule AdventOfCode2021.Day2 do
-  def follow_course(course, horizontal \\ 0, depth \\ 0)
+  def follow_course(course, horizontal \\ 0, depth \\ 0, aim \\ 0)
 
-  def follow_course(["forward " <> x | rest], horizontal, depth) when depth >= 0 do
+  def follow_course(["forward " <> x | rest], horizontal, depth, aim) when depth >= 0 do
     num = to_num(x)
-    follow_course(rest, horizontal + num, depth)
+    follow_course(rest, horizontal + num, depth + aim * num, aim)
   end
 
-  def follow_course(["down " <> x | rest], horizontal, depth) when depth >= 0 do
+  def follow_course(["down " <> x | rest], horizontal, depth, aim) when depth >= 0 do
     num = to_num(x)
-    follow_course(rest, horizontal, depth + num)
+    follow_course(rest, horizontal, depth, aim + num)
   end
 
-  def follow_course(["up " <> x | rest], horizontal, depth) when depth >= 0 do
+  def follow_course(["up " <> x | rest], horizontal, depth, aim) when depth >= 0 do
     num = to_num(x)
-    follow_course(rest, horizontal, depth - num)
+    follow_course(rest, horizontal, depth, aim - num)
   end
 
-  def follow_course([], horizontal, depth), do: {horizontal, depth}
+  def follow_course([], horizontal, depth, aim), do: {horizontal, depth, aim}
 
   defp to_num(string) do
     case Integer.parse(string) do
--- test/problems/day2_test.exs
+++ test/problems/day2_test.exs
@@ -4,13 +4,13 @@ defmodule AdventOfCode2021.Day2Test do
 
   test "follow course" do
     assert follow_course(["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]) ==
-             {15, 10}
+             {15, 60, 10}
   end
 
   @input ReadInput.as_strings(2)
   test "puzzle input" do
-    # {1890, 1172}
-    {horizontal, depth} = follow_course(@input)
-    assert horizontal * depth == 2_215_080
+    # {1890, 986_622, 1172}
+    {horizontal, depth, 1172} = follow_course(@input)
+    assert horizontal * depth == 1_864_715_580
   end
 end
