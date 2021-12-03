6971128fbf4cc516990346bb34e57d52ce153e95
matt <mgwidmann@gmail.com>
2021-12-02 21:35:32 -0500

    day 1

---
+++ lib/advent_of_code2021/problems/day1.ex
@@ -0,0 +1,16 @@
+defmodule AdventOfCode2021.Day1 do
+
+  @spec count_depth_increases(nonempty_list, number | nil, number) :: no_return
+  def count_depth_increases(list, prev \\ nil, increases \\ 0)
+  def count_depth_increases([depth | rest], nil, increases) do
+    count_depth_increases(rest, depth, increases)
+  end
+  def count_depth_increases([depth | rest], prev, increases) when depth > prev do
+    count_depth_increases(rest, depth, increases + 1)
+  end
+  def count_depth_increases([depth | rest], _prev, increases) do
+    count_depth_increases(rest, depth, increases)
+  end
+  def count_depth_increases([], _prev, increases), do: increases
+
+end
---
+++ test/support/read_input.ex
@@ -0,0 +1,14 @@
+defmodule ReadInput do
+  def as_numbers(day) do
+    "test/inputs/day#{day}.txt"
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(fn s -> {s, s |> String.trim() |> Integer.parse()} end)
+    |> Enum.map(fn {string, result} ->
+      if match?(:error, result) do
+        raise "Unable to parse input as a number: #{string}"
+      end
+      result
+    end)
+  end
+end
