c29980aeaca937dc4143efda6b04418f295f2537
matt <mgwidmann@gmail.com>
2021-12-02 22:02:38 -0500

    Day 1 part 2

--- lib/advent_of_code2021/problems/day1.ex
+++ lib/advent_of_code2021/problems/day1.ex
@@ -1,16 +1,21 @@
 defmodule AdventOfCode2021.Day1 do
-
-  @spec count_depth_increases(nonempty_list, number | nil, number) :: number
+  @spec count_depth_increases(nonempty_list, {number, number, number} | nil, number) :: number
   def count_depth_increases(list, prev \\ nil, increases \\ 0)
-  def count_depth_increases([depth | rest], nil, increases) do
-    count_depth_increases(rest, depth, increases)
-  end
-  def count_depth_increases([depth | rest], prev, increases) when depth > prev do
-    count_depth_increases(rest, depth, increases + 1)
-  end
-  def count_depth_increases([depth | rest], _prev, increases) do
-    count_depth_increases(rest, depth, increases)
-  end
-  def count_depth_increases([], _prev, increases), do: increases
 
+  def count_depth_increases([first, second, third | rest], nil, increases) do
+    count_depth_increases([second, third | rest], {first, second, third}, increases)
+  end
+
+  def count_depth_increases([first, second, third | rest], {prev1, prev2, prev3}, increases)
+      when first + second + third > prev1 + prev2 + prev3 do
+    count_depth_increases([second, third | rest], {first, second, third}, increases + 1)
+  end
+
+  def count_depth_increases([first, second, third | rest], _prev, increases) do
+    count_depth_increases([second, third | rest], {first, second, third}, increases)
+  end
+
+  def count_depth_increases([_first, _second], _prev, increases), do: increases
+  def count_depth_increases([_first], _prev, increases), do: increases
+  def count_depth_increases([], _prev, increases), do: increases
 end
--- test/problems/day1_test.exs
+++ test/problems/day1_test.exs
@@ -3,11 +3,11 @@ defmodule AdventOfCode2021.Day1Test do
   import AdventOfCode2021.Day1
 
   test "count depth increases" do
-    assert count_depth_increases([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 7
+    assert count_depth_increases([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 5
   end
 
   @input ReadInput.as_numbers(1)
   test "puzzle input" do
-    assert count_depth_increases(@input) == 1559
+    assert count_depth_increases(@input) == 1600
   end
 end
