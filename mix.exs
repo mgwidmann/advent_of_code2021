98763c9513b1cab3ea8a9ad57cc8e10e643d1015
matt <mgwidmann@gmail.com>
2021-12-02 21:25:05 -0500

    Start project.

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule AdventOfCode2021.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent_of_code2021,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {AdventOfCode2021.Application, []}
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
